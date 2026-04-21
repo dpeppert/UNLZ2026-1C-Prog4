7052e33b289207fdc23c67549866a3056de20fe7
diegopeppert <dpeppert@cfi.org.ar>
2026-04-21 19:04:58 -0300

    Commit temporal

---
+++ Web/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Models;
+
+namespace Web.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<EventoVM> Eventos { get; set; }
+
+    }
+}
--- Web/Models/EventoVM.cs
+++ Web/Models/EventoVM.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Models
 {
     public class EventoVM
-    { 
-    
+    {
+//        [Key]
         public int IdEvento { get; set; }
 
         public string NombreEvento { get; set; }
